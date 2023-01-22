f260323a3c4c9c8f25fc229be6d14418de4b06fe
henriquekiki <ricckdmo@gmail.com>
2023-01-22 16:20:00 -0300

    Exercicios

---
+++ EstruturaBasicaDaOo/src/RodarAplicacao.java
@@ -0,0 +1,23 @@
+class RodarAplicacao {
+    /* 
+    public static void main(String[] args) {
+        
+        Carro carro1 = new Carro();
+        
+        carro1.setCor("Branco");
+        carro1.setModelo("Onix");
+        carro1.setCapacidadeTanque(54);
+
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.totalValorTanque(4.94));
+
+        Carro carro2 = new Carro("Preto", "Corolla Hybrid", 60);
+
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.totalValorTanque(4.85));
+    }*/
+}
