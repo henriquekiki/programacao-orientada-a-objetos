7dfb4c6436dd86ff563d53e2bbe758bf75ebe1b7
henriquekiki <ricckdmo@gmail.com>
2023-01-22 17:07:24 -0300

    Exercicio de herança

---
+++ Exerciocio/Exercicio3/src/ClasseFilha2.java
@@ -0,0 +1,10 @@
+class ClasseFilha2 extends ClasseMae{
+    
+    void metodo1(){
+        System.out.println("Método 1 da Classe Filha 2");
+    }
+
+    void metodo2(){
+        System.out.println("Método 2 da Classe Filha 2");
+    }
+}
