b8f2c139c401831a1406633c3a4660d8b7a3ed23
henriquekiki <ricckdmo@gmail.com>
2023-01-22 16:19:52 -0300

    Exercicios

---
+++ EstruturaBasicaDaOo/src/Carro.java
@@ -0,0 +1,70 @@
+class Carro extends Veiculo{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* 
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    Carro(){
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor=cor;
+        this.modelo=modelo;
+        this.capacidadeTanque=capacidadeTanque;
+
+    }
+
+    //Cor
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    } 
+    //Modelo
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    String getModelo(){
+        return modelo;
+    }
+    //Tanque
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque(){
+        return capacidadeTanque;
+    }
+    //Calculo do tanque
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
+*/
+
+}
